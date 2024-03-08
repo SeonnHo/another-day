50e37ae1cc87003855d9c383c3da820702c102e0
SeonnHo <prefer914@gmail.com>
2024-03-08 21:16:14 +0900

    Header 공통 컴포넌트 기본적인 UI 구현

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import S from './Header.styled';
+
+export default function Header() {
+  return (
+    <S.HeaderLayout>
+      <p>header</p>
+    </S.HeaderLayout>
+  );
+}
---
+++ src/components/Header/Header.styled.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const S = {};
+
+S.HeaderLayout = styled.header`
+  background-color: lightgray;
+  display: flex;
+  width: 100%;
+  height: 80px;
+`;
+
+export default S;
