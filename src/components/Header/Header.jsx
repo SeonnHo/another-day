478686d02589ccf3250da26709791440251e0469
SeonnHo <prefer914@gmail.com>
2024-03-10 01:28:03 +0900

    어느 페이지에서나 보여지는 공통 헤더 컴포넌트 UI 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './Header.style';
+import logo from '../../assets/another_day_logo.png';
+import useWindowSize from '../../hooks/useWindowSize';
+import { FiMenu } from 'react-icons/fi';
+import NavDrawer from '../NavDrawer/NavDrawer';
 
 export default function Header() {
+  const windowSize = useWindowSize();
+  const [isShowBackdrop, setIsShowBackdrop] = useState(false);
+  const [isCloseDrawer, setIsCloseDrawer] = useState(false);
+
+  const openDrawer = () => {
+    setIsShowBackdrop(true);
+  };
+
+  const closeDrawer = () => {
+    setIsCloseDrawer(true);
+    setTimeout(() => {
+      setIsShowBackdrop(false);
+      setIsCloseDrawer(false);
+    }, 600);
+  };
+
   return (
     <S.HeaderLayout>
-      <p>header</p>
+      <S.HeaderContainer>
+        <S.HeaderLogoBox>
+          <S.HeaderLogoImg src={logo} />
+          <S.HeaderLogoH1>Another Day</S.HeaderLogoH1>
+        </S.HeaderLogoBox>
+        {windowSize.width > 1024 && (
+          <S.HeaderNav>
+            <S.HeaderList>
+              <S.HeaderItem>home</S.HeaderItem>
+              <S.HeaderItem>products</S.HeaderItem>
+              <S.HeaderItem>about</S.HeaderItem>
+              <S.HeaderItem>my page</S.HeaderItem>
+            </S.HeaderList>
+          </S.HeaderNav>
+        )}
+
+        {windowSize.width < 1024 && (
+          <>
+            <S.HeaderButton onClick={openDrawer}>
+              <FiMenu />
+            </S.HeaderButton>
+            {isShowBackdrop && (
+              <NavDrawer
+                isCloseDrawer={isCloseDrawer}
+                closeDrawer={closeDrawer}
+              />
+            )}
+          </>
+        )}
+      </S.HeaderContainer>
     </S.HeaderLayout>
   );
 }
--- src/components/Header/Header.style.js
+++ src/components/Header/Header.style.js
@@ -1,8 +1,114 @@
 import styled from 'styled-components';
 
 export const HeaderLayout = styled.header`
-  background-color: lightgray;
   display: flex;
-  width: 100%;
+  justify-content: center;
+  align-items: center;
+  background-color: #ffbe98;
+  width: 100vw;
   height: 80px;
+`;
+
+export const HeaderContainer = styled.div`
+  width: 1100px;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media screen and (max-width: 1024px) {
+    width: 100%;
+    margin-left: 1rem;
+  }
+`;
+
+export const HeaderLogoBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const HeaderLogoImg = styled.img`
+  width: 3rem;
+  height: 3rem;
+  padding-bottom: 1rem;
+
+  @media screen and (max-width: 1024px) {
+    width: 2rem;
+    height: 2rem;
+  }
+`;
+
+export const HeaderLogoH1 = styled.h1`
+  font-size: 1.5rem;
+  font-family: sans-serif;
+  font-weight: bold;
+  color: white;
+  margin: 0;
+  white-space: nowrap;
+
+  @media screen and (max-width: 1024px) {
+    font-size: 1rem;
+  }
+`;
+
+export const HeaderNav = styled.nav`
+  display: flex;
+  align-items: center;
+`;
+
+export const HeaderList = styled.ul`
+  padding: 0;
+  margin: 0;
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderItem = styled.li`
+  color: #f2e8da;
+  font-size: 1.2rem;
+  font-family: sans-serif;
+  font-weight: bold;
+  white-space: nowrap;
+  margin-left: 2rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  &:hover {
+    color: #de8286;
+  }
+
+  @media screen and (max-width: 1024px) {
+    font-size: 0.8rem;
+  }
+`;
+
+export const HeaderButton = styled.button`
+  border: none;
+  outline: none;
+  background-color: transparent;
+  color: white;
+  padding: 0;
+  margin-right: 1rem;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+  flex-shrink: 0;
+
+  & > svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  @media screen and (max-width: 1024px) {
+    width: 1.2rem;
+    height: 1.2rem;
+
+    & > svg {
+      width: 1.2rem;
+      height: 1.2rem;
+    }
+  }
 `;
