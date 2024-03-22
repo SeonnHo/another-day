71b6b8cc51b8d5184eb5a60b53819267d06f05de
SeonnHo <prefer914@gmail.com>
2024-03-22 22:39:30 +0900

    firebase 위치 변경 및 로그인, 로그아웃, 관리자검증, 상품 등록 로직 추가

---
+++ src/api/firebase.js
@@ -0,0 +1,64 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { get, getDatabase, ref, set } from 'firebase/database';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+import { v4 as uuid } from 'uuid';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const database = getDatabase(app);
+
+export function login() {
+  signInWithPopup(auth, provider).catch(console.error);
+}
+
+export function logout() {
+  signOut(auth).catch(console.error);
+}
+
+export function onUserStateChange(callback) {
+  onAuthStateChanged(auth, async (user) => {
+    const updatedUser = user ? await adminUser(user) : null;
+    callback(updatedUser);
+  });
+}
+
+async function adminUser(user) {
+  return get(ref(database, 'admins')).then((snapshot) => {
+    if (snapshot.exists()) {
+      const admins = snapshot.val();
+      const isAdmin = admins.includes(user.uid);
+      return { ...user, isAdmin };
+    }
+    return user;
+  });
+}
+
+export async function addNewProduct(product, image) {
+  const id = uuid();
+  return set(ref(database, `products/${id}`), {
+    ...product,
+    id,
+    price: parseInt(product.price),
+    image,
+    size: product.size.split(','),
+  });
+}
+
+export function useFirebase() {
+  return { adminUser, auth };
+}
