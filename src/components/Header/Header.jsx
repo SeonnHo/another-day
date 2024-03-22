60eecd284a581f0c8cadf265cce4d00dc30c5199
SeonnHo <prefer914@gmail.com>
2024-03-22 22:44:22 +0900

    서비스 로직을 firebase로 이동 후 컴포넌트 내 해당 로직 삭제

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,80 +1,36 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import * as S from './Header.style';
 import { FiShoppingCart, FiEdit2 } from 'react-icons/fi';
-import { auth } from '../../firebase';
-import {
-  GoogleAuthProvider,
-  browserSessionPersistence,
-  setPersistence,
-  signInWithPopup,
-  signOut,
-} from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../context/AuthContext';
 
 export default function Header() {
-  const [isLogined, setIsLogined] = useState(false);
+  const { user, login, logout } = useAuthContext();
   const navigate = useNavigate();
-
-  const handleGoogleSignIn = () => {
-    const provider = new GoogleAuthProvider();
-    setPersistence(auth, browserSessionPersistence)
-      .then(() => {
-        signInWithPopup(auth, provider)
-          .then(() => {
-            setIsLogined(true);
-          })
-          .catch((error) => console.log(error));
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const handleGoogleSignOut = () => {
-    signOut(auth)
-      .then(() => {
-        setIsLogined(false);
-        navigate('/');
-      })
-      .catch((error) => console.log(error));
-  };
-
-  const sessionUserData = () => {
-    for (const key of Object.keys(sessionStorage)) {
-      if (key.includes('firebase:authUser')) {
-        return JSON.parse(sessionStorage.getItem(key));
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (sessionUserData()) {
-      setIsLogined(true);
-    } else {
-      setIsLogined(false);
-    }
-  }, []);
 
   return (
     <S.HeaderLayout>
       <S.HeaderContainer>
         <S.HeaderLogoBox onClick={() => navigate('/')}>
-          <S.HeaderLogoImg src={'assets/another_day_logo.png'} />
+          <S.HeaderLogoImg src={'/assets/another_day_logo.png'} />
           <S.HeaderLogoH1>Another Day</S.HeaderLogoH1>
         </S.HeaderLogoBox>
         <S.HeaderNav>
           <S.HeaderList>
             <S.HeaderItem>products</S.HeaderItem>
-            <S.HeaderItem>
-              <S.HeaderIconButton>
-                <FiShoppingCart />
-              </S.HeaderIconButton>
-            </S.HeaderItem>
-            {isLogined ? (
+            {user ? (
               <>
-                {sessionUserData().uid === process.env.REACT_APP_ADMIN_UID && (
+                <S.HeaderItem>
+                  <S.HeaderIconButton>
+                    <FiShoppingCart />
+                  </S.HeaderIconButton>
+                </S.HeaderItem>
+
+                {user.isAdmin && (
                   <S.HeaderItem>
                     <S.HeaderIconButton
                       onClick={() => {
-                        navigate('/create');
+                        navigate('/products/new');
                       }}
                     >
                       <FiEdit2 />
@@ -84,21 +40,19 @@ export default function Header() {
 
                 <S.HeaderUserInfoItem>
                   <S.HeaderUserAvatarImg
-                    src={sessionUserData().photoURL}
+                    src={user.photoURL}
                     alt="user profile img"
                   />
                 </S.HeaderUserInfoItem>
                 <S.HeaderItem>
-                  <S.HeaderSignButton onClick={handleGoogleSignOut}>
+                  <S.HeaderSignButton onClick={logout}>
                     로그아웃
                   </S.HeaderSignButton>
                 </S.HeaderItem>
               </>
             ) : (
               <S.HeaderItem>
-                <S.HeaderSignButton onClick={handleGoogleSignIn}>
-                  로그인
-                </S.HeaderSignButton>
+                <S.HeaderSignButton onClick={login}>로그인</S.HeaderSignButton>
               </S.HeaderItem>
             )}
           </S.HeaderList>
