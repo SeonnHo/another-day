b6fca6d406e4aa2e4cbb7e5004e6e510c4eba96d
SeonnHo <prefer914@gmail.com>
2024-03-22 22:43:02 +0900

    AuthContextProvider 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
+import { AuthContextProvider } from './components/context/AuthContext';
 
 export default function App() {
   return (
-    <>
+    <AuthContextProvider>
       <Header />
       <Outlet />
-    </>
+    </AuthContextProvider>
   );
 }
