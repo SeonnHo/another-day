a39317c5347c8c2bb466382b6ecd0f1137362171
SeonnHo <prefer914@gmail.com>
2024-03-22 22:46:46 +0900

    텍스트필드 컴포넌트 props 추가

--- src/components/TextField/TextField.jsx
+++ src/components/TextField/TextField.jsx
@@ -8,16 +8,22 @@ export default function TextField({
   fontSize,
   decorationColor,
   textColor,
+  value,
+  handleTextChange,
+  name,
 }) {
   return (
     <S.TextFieldBox>
       <S.TextFieldInput
         id={id}
         type={type}
+        name={name}
         $textColor={textColor}
         $fontSize={fontSize}
         $decorationColor={decorationColor}
         required
+        value={value}
+        onChange={handleTextChange}
       />
       <S.TextFieldLabel htmlFor={id} $fontSize={fontSize}>
         {children}
