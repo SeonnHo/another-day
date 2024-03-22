bce65a6e800cd3f2620e20b59c48a8af7d2d11da
SeonnHo <prefer914@gmail.com>
2024-03-22 22:42:25 +0900

    user 정보와 login logout 로직을 전역적으로 사용하기위한 AuthContextAPI 구현

---
+++ src/components/context/AuthContext.jsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { login, logout, useFirebase } from '../../api/firebase';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = createContext();
+
+export function AuthContextProvider({ children }) {
+  const [authState, setAuthState] = useState({ user: null, loading: true });
+  const { adminUser, auth } = useFirebase();
+  const user = authState.user;
+  const loading = authState.loading;
+
+  useEffect(() => {
+    const stopListen = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        adminUser(user).then((user) => setAuthState({ user, loading: false }));
+      } else {
+        setAuthState({ user: null, loading: false });
+      }
+    });
+    return () => stopListen();
+  }, [adminUser, auth]);
+
+  return (
+    <AuthContext.Provider value={{ user, loading, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuthContext() {
+  return useContext(AuthContext);
+}
