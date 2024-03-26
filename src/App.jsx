daf9bf79ddcbca057ab759fd2781925f60d6b856
SeonnHo <prefer914@gmail.com>
2024-03-26 19:47:52 +0900

    QueryClient 및 ReactQueryDevtools 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
 import { AuthContextProvider } from './components/context/AuthContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App() {
   return (
     <AuthContextProvider>
       <Header />
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </AuthContextProvider>
   );
 }
