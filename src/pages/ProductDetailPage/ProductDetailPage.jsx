034a0adb6617a6b3f5a1de88ec8412cb66e67da3
SeonnHo <prefer914@gmail.com>
2024-03-27 18:28:41 +0900

    상품 상세 페이지 UI 구현

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,5 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as S from './ProductDetailPage.style';
+import { useLocation } from 'react-router-dom';
 
 export default function ProductDetailPage() {
-  return <div>product detail page</div>;
+  const { state: product } = useLocation();
+
+  const [selected, setSelected] = useState('S');
+
+  const handleChange = (e) => {
+    setSelected(e.target.value);
+  };
+
+  const addExtraPriceBySize = (price, size) => {
+    switch (size) {
+      case 'S':
+        return price;
+
+      case 'M':
+        return price + 500;
+
+      case 'L':
+        return price + 1000;
+
+      default:
+        return price;
+    }
+  };
+
+  return (
+    <S.ProductDetailPageLayout>
+      <S.ProductDetailPageProductImageSection>
+        <S.ProductDetailPageProductImg
+          src={product.image}
+          alt={`${product.title} 사진`}
+        />
+      </S.ProductDetailPageProductImageSection>
+
+      <S.ProductDetailPageProductInfoSection>
+        <S.ProductDetailPageProductTitle>
+          {product.title}
+        </S.ProductDetailPageProductTitle>
+        <S.ProductDetailPageProductDescription>
+          {product.description}
+        </S.ProductDetailPageProductDescription>
+        <S.ProductDetailPageColumnBox>
+          <S.ProductDetailPageProductPriceBox>
+            <S.ProductDetailPageProductSizeSelect
+              value={selected}
+              onChange={handleChange}
+            >
+              <optgroup label="사이즈">
+                {product.size.map((size) => {
+                  return (
+                    <option key={size} value={size}>
+                      {size}
+                    </option>
+                  );
+                })}
+              </optgroup>
+            </S.ProductDetailPageProductSizeSelect>
+            <S.ProductDetailPageProductPrice>
+              {addExtraPriceBySize(product.price, selected).toLocaleString(
+                'ko-KR'
+              )}
+              원
+            </S.ProductDetailPageProductPrice>
+          </S.ProductDetailPageProductPriceBox>
+
+          <S.ProductDetailPageButton>장바구니 추가</S.ProductDetailPageButton>
+        </S.ProductDetailPageColumnBox>
+      </S.ProductDetailPageProductInfoSection>
+    </S.ProductDetailPageLayout>
+  );
 }
---
+++ src/pages/ProductDetailPage/ProductDetailPage.style.js
@@ -0,0 +1,108 @@
+import styled from 'styled-components';
+
+export const ProductDetailPageLayout = styled.main`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-family: 'Ownglyph';
+
+  @media screen and (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+export const ProductDetailPageProductImageSection = styled.section`
+  width: 400px;
+  margin-right: 5rem;
+
+  @media screen and (max-width: 768px) {
+    width: 200px;
+    margin: 0;
+  }
+`;
+
+export const ProductDetailPageProductImg = styled.img`
+  width: 100%;
+`;
+
+export const ProductDetailPageProductInfoSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  border: 2px dashed #ffbe98;
+  width: 400px;
+  padding: 1rem;
+  height: 400px;
+
+  @media screen and (max-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+`;
+
+export const ProductDetailPageProductTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 1rem 0;
+  text-align: center;
+`;
+
+export const ProductDetailPageProductDescription = styled.p`
+  font-size: 1.2rem;
+  color: gray;
+  margin: 1rem 0;
+  text-align: center;
+`;
+
+export const ProductDetailPageProductPriceBox = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ProductDetailPageProductSizeSelect = styled.select`
+  outline: none;
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+  border: 1px solid black;
+  flex-grow: 1;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-color: white;
+  color: black;
+  background-image: url('/assets/arrow.png');
+  background-repeat: no-repeat;
+  background-position-x: 98%;
+  background-position-y: 50%;
+`;
+
+export const ProductDetailPageProductPrice = styled.p`
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: end;
+  margin: 0;
+  flex-grow: 1;
+`;
+
+export const ProductDetailPageColumnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProductDetailPageButton = styled.button`
+  border: none;
+  padding: 0.7rem 0;
+  width: 100%;
+  background-color: #ffbe98;
+  color: white;
+  font-weight: bold;
+  border-radius: 0.5rem;
+  margin-top: 0.5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fa9a85;
+  }
+`;
