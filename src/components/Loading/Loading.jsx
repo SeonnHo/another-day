1001282c2ca9061122a8c2bb0feb546e0865cde6
SeonnHo <prefer914@gmail.com>
2024-03-27 18:27:36 +0900

    로딩 컴포넌트 props 추가

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import * as S from './Loading.style';
 import { createPortal } from 'react-dom';
 
-export default function Loading() {
+export default function Loading({ children }) {
   return (
     <>
       {createPortal(
         <S.LoadingBackdrop>
           <S.LoadingBox>
-            <S.LoadingText>상품 등록 중입니다.</S.LoadingText>
+            <S.LoadingText>{children}</S.LoadingText>
             <S.LoadingImg src={'/assets/spinner.gif'} alt="spinner" />
           </S.LoadingBox>
         </S.LoadingBackdrop>,
