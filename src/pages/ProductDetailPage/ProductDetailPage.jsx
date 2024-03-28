c26e79a905f926c1d64a0172a3bfd10ade79fbaa
SeonnHo <prefer914@gmail.com>
2024-03-29 00:00:15 +0900

    장바구니 추가 상품 localstorage에 저장하는 로직 구현 및 로딩 및 성공 상태 UI 구현

--- src/pages/ProductDetailPage/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage/ProductDetailPage.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import * as S from './ProductDetailPage.style';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import Loading from '../../components/Loading/Loading';
+import Modal from '../../components/Modal/Modal';
 
 export default function ProductDetailPage() {
   const { state: product } = useLocation();
+  const navigate = useNavigate();
 
   const [selected, setSelected] = useState('S');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
 
   const handleChange = (e) => {
     setSelected(e.target.value);
@@ -27,49 +32,108 @@ export default function ProductDetailPage() {
     }
   };
 
+  const handleAddToMyCart = () => {
+    setIsLoading(true);
+
+    const storageItem = localStorage.getItem('cartItem');
+    const { id, image, title, price } = product;
+    if (!storageItem) {
+      localStorage.setItem(
+        'cartItem',
+        JSON.stringify([
+          {
+            id,
+            image,
+            title,
+            price: addExtraPriceBySize(price, selected),
+            totalPrice: addExtraPriceBySize(price, selected),
+            size: selected,
+            count: 1,
+          },
+        ])
+      );
+    } else {
+      const item = JSON.parse(storageItem);
+      item.push({
+        id,
+        image,
+        title,
+        price: addExtraPriceBySize(price, selected),
+        totalPrice: addExtraPriceBySize(price, selected),
+        size: selected,
+        count: 1,
+      });
+      const data = JSON.stringify(item);
+      localStorage.setItem('cartItem', data);
+    }
+    setTimeout(() => {
+      setIsLoading(false);
+      setIsSuccess(true);
+    }, 1000);
+  };
+
+  const handleMoveToMyCartPage = () => {
+    navigate('/carts', { replace: true });
+  };
+
   return (
-    <S.ProductDetailPageLayout>
-      <S.ProductDetailPageProductImageSection>
-        <S.ProductDetailPageProductImg
-          src={product.image}
-          alt={`${product.title} 사진`}
-        />
-      </S.ProductDetailPageProductImageSection>
+    <>
+      <S.ProductDetailPageLayout>
+        <S.ProductDetailPageProductImageSection>
+          <S.ProductDetailPageProductImg
+            src={product.image}
+            alt={`${product.title} 사진`}
+          />
+        </S.ProductDetailPageProductImageSection>
 
-      <S.ProductDetailPageProductInfoSection>
-        <S.ProductDetailPageProductTitle>
-          {product.title}
-        </S.ProductDetailPageProductTitle>
-        <S.ProductDetailPageProductDescription>
-          {product.description}
-        </S.ProductDetailPageProductDescription>
-        <S.ProductDetailPageColumnBox>
-          <S.ProductDetailPageProductPriceBox>
-            <S.ProductDetailPageProductSizeSelect
-              value={selected}
-              onChange={handleChange}
-            >
-              <optgroup label="사이즈">
-                {product.size.map((size) => {
-                  return (
-                    <option key={size} value={size}>
-                      {size}
-                    </option>
-                  );
-                })}
-              </optgroup>
-            </S.ProductDetailPageProductSizeSelect>
-            <S.ProductDetailPageProductPrice>
-              {addExtraPriceBySize(product.price, selected).toLocaleString(
-                'ko-KR'
-              )}
-              원
-            </S.ProductDetailPageProductPrice>
-          </S.ProductDetailPageProductPriceBox>
+        <S.ProductDetailPageProductInfoSection>
+          <S.ProductDetailPageProductTitle>
+            {product.title}
+          </S.ProductDetailPageProductTitle>
+          <S.ProductDetailPageProductDescription>
+            {product.description}
+          </S.ProductDetailPageProductDescription>
+          <S.ProductDetailPageColumnBox>
+            <S.ProductDetailPageProductPriceBox>
+              <S.ProductDetailPageProductSizeSelect
+                value={selected}
+                onChange={handleChange}
+              >
+                <optgroup label="사이즈">
+                  {product.size.map((size) => {
+                    return (
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    );
+                  })}
+                </optgroup>
+              </S.ProductDetailPageProductSizeSelect>
+              <S.ProductDetailPageProductPrice>
+                {addExtraPriceBySize(product.price, selected).toLocaleString(
+                  'ko-KR'
+                )}
+                원
+              </S.ProductDetailPageProductPrice>
+            </S.ProductDetailPageProductPriceBox>
 
-          <S.ProductDetailPageButton>장바구니 추가</S.ProductDetailPageButton>
-        </S.ProductDetailPageColumnBox>
-      </S.ProductDetailPageProductInfoSection>
-    </S.ProductDetailPageLayout>
+            <S.ProductDetailPageButton onClick={handleAddToMyCart}>
+              장바구니 추가
+            </S.ProductDetailPageButton>
+          </S.ProductDetailPageColumnBox>
+        </S.ProductDetailPageProductInfoSection>
+      </S.ProductDetailPageLayout>
+      {isLoading && <Loading>장바구니에 담고 있어요.</Loading>}
+      {isSuccess && (
+        <Modal
+          onConfirm={handleMoveToMyCartPage}
+          onCancel={() => setIsSuccess(false)}
+        >
+          장바구니에 잘 담았어요!
+          <br />
+          장바구니 페이지로 이동할까요?
+        </Modal>
+      )}
+    </>
   );
 }
