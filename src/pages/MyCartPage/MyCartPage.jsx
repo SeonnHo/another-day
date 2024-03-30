bec8810aa7362110ae5924ae0036b898b477276d
SeonnHo <prefer914@gmail.com>
2024-03-30 23:41:14 +0900

    장바구니 페이지 UI 구현

--- src/pages/MyCartPage/MyCartPage.jsx
+++ src/pages/MyCartPage/MyCartPage.jsx
@@ -1,5 +1,189 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import * as S from './MyCartPage.style';
+import { FiMinusCircle, FiPlusCircle } from 'react-icons/fi';
+import { RiDeleteBin5Fill } from 'react-icons/ri';
+
+const MINUS = 'minus';
+const PLUS = 'plus';
 
 export default function MyCartPage() {
-  return <div>my cart page</div>;
+  const [itemList, setItemList] = useState([]);
+
+  useEffect(() => {
+    const storageItem = localStorage.getItem('cartItem');
+    if (storageItem) {
+      setItemList(JSON.parse(storageItem));
+    }
+  }, []);
+
+  const handleItemCount = (type, cartItem) => {
+    switch (type) {
+      case MINUS:
+        setItemList((prev) =>
+          prev.map((item, index) => {
+            if (
+              item.title === cartItem.title &&
+              item.size === cartItem.size &&
+              cartItem.count > 1
+            ) {
+              const storageItemList = JSON.parse(
+                localStorage.getItem('cartItem')
+              );
+              storageItemList[index].count = item.count - 1;
+              storageItemList[index].totalPrice = item.price * (item.count - 1);
+              localStorage.setItem('cartItem', JSON.stringify(storageItemList));
+              return {
+                ...item,
+                count: item.count - 1,
+                totalPrice: item.price * (item.count - 1),
+              };
+            }
+            return item;
+          })
+        );
+        break;
+
+      case PLUS:
+        setItemList((prev) =>
+          prev.map((item, index) => {
+            if (item.title === cartItem.title && item.size === cartItem.size) {
+              const storageItemList = JSON.parse(
+                localStorage.getItem('cartItem')
+              );
+              storageItemList[index].count = item.count + 1;
+              storageItemList[index].totalPrice = item.price * (item.count + 1);
+              localStorage.setItem('cartItem', JSON.stringify(storageItemList));
+              return {
+                ...item,
+                count: item.count + 1,
+                totalPrice: item.price * (item.count + 1),
+              };
+            }
+            return item;
+          })
+        );
+        break;
+
+      default:
+        return;
+    }
+  };
+
+  const handleRemoveItem = (cartItem) => {
+    setItemList((prev) =>
+      prev.filter((item, index) => {
+        if (item.title !== cartItem && item.size !== cartItem.size) {
+          return true;
+        }
+        const storageItemList = JSON.parse(localStorage.getItem('cartItem'));
+        console.log(storageItemList);
+        const filterList = storageItemList.filter((_, idx) => index !== idx);
+        localStorage.setItem('cartItem', JSON.stringify(filterList));
+        return false;
+      })
+    );
+  };
+
+  const calculateTotalPrice = () => {
+    const priceList = itemList.map((item) => item.totalPrice);
+    console.log(priceList);
+    let total = 0;
+    for (const price of priceList) {
+      total = total + price;
+      console.log(total);
+    }
+
+    return total;
+  };
+
+  return (
+    <S.MyCartPageLayout>
+      {itemList.length === 0 ? (
+        <S.MyCartPageNotFoundMessageSection>
+          <S.MyCartPageNotFoundMessage>
+            장바구니에 상품을 담지 않았어요.
+          </S.MyCartPageNotFoundMessage>
+        </S.MyCartPageNotFoundMessageSection>
+      ) : (
+        <>
+          <S.MyCartPageItemListSection>
+            {itemList.map((item) => {
+              return (
+                <S.MyCartPageItemBox key={item.id + item.size}>
+                  <S.MyCartPageItemImg
+                    src={item.image}
+                    alt={`${item.title} 사진`}
+                  />
+                  <S.MyCartPageItemInfoBox>
+                    <S.MyCartPageItemTitle>{item.title}</S.MyCartPageItemTitle>
+                    <S.MyCartPageItemSize>{item.size}</S.MyCartPageItemSize>
+                    <S.MyCartPageItemPrice>
+                      {item.totalPrice?.toLocaleString('ko-KR')}원
+                    </S.MyCartPageItemPrice>
+                  </S.MyCartPageItemInfoBox>
+
+                  <S.MyCartPageItemControlBox>
+                    <S.MyCartPageItemControlButton
+                      onClick={() => handleItemCount(MINUS, item)}
+                    >
+                      <FiMinusCircle />
+                    </S.MyCartPageItemControlButton>
+
+                    <S.MyCartPageItemCount>{item.count}</S.MyCartPageItemCount>
+
+                    <S.MyCartPageItemControlButton
+                      onClick={() => handleItemCount(PLUS, item)}
+                    >
+                      <FiPlusCircle />
+                    </S.MyCartPageItemControlButton>
+
+                    <S.MyCartPageItemControlButton
+                      onClick={() => handleRemoveItem(item)}
+                    >
+                      <RiDeleteBin5Fill />
+                    </S.MyCartPageItemControlButton>
+                  </S.MyCartPageItemControlBox>
+                </S.MyCartPageItemBox>
+              );
+            })}
+          </S.MyCartPageItemListSection>
+
+          <S.MyCartPageOrderSection>
+            <S.MyCartPageOrderBox>
+              <S.MyCartPageOrderFlexBox>
+                <S.MyCartPageOrderPriceText>
+                  주문 금액
+                </S.MyCartPageOrderPriceText>
+                <S.MyCartPageOrderPriceText>
+                  {calculateTotalPrice().toLocaleString('ko-KR')}원
+                </S.MyCartPageOrderPriceText>
+              </S.MyCartPageOrderFlexBox>
+              <S.MyCartPageOrderFlexBox>
+                <S.MyCartPageOrderPriceText>배달비</S.MyCartPageOrderPriceText>
+                <S.MyCartPageOrderPriceText>
+                  {(3000).toLocaleString('ko-KR')}원
+                </S.MyCartPageOrderPriceText>
+              </S.MyCartPageOrderFlexBox>
+
+              <S.MyCartPageDivider />
+
+              <S.MyCartPageOrderFlexBox>
+                <S.MyCartPageOrderTotalPriceText>
+                  결제 금액
+                </S.MyCartPageOrderTotalPriceText>
+                <S.MyCartPageOrderTotalPriceText>
+                  {(calculateTotalPrice() + 3000).toLocaleString('ko-KR')}원
+                </S.MyCartPageOrderTotalPriceText>
+              </S.MyCartPageOrderFlexBox>
+            </S.MyCartPageOrderBox>
+            <S.MyCartPageOrderButton>
+              {' '}
+              {(calculateTotalPrice() + 3000).toLocaleString('ko-KR')}
+              원&nbsp;•&nbsp;주문하기
+            </S.MyCartPageOrderButton>
+          </S.MyCartPageOrderSection>
+        </>
+      )}
+    </S.MyCartPageLayout>
+  );
 }
---
+++ src/pages/MyCartPage/MyCartPage.style.js
@@ -0,0 +1,222 @@
+import styled from 'styled-components';
+
+export const MyCartPageLayout = styled.main`
+  width: 100%;
+  min-height: calc(100vh - 100px);
+  font-family: 'Ownglyph';
+
+  @media screen and (max-width: 768px) {
+    min-height: calc(100vh - 80px);
+  }
+`;
+
+export const MyCartPageNotFoundMessageSection = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MyCartPageNotFoundMessage = styled.h2`
+  margin: 0;
+  text-align: center;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const MyCartPageItemListSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+
+  @media screen and (max-width: 768px) {
+    margin-top: 80px;
+  }
+`;
+
+export const MyCartPageItemBox = styled.div`
+  display: flex;
+  margin: 1.5rem 0 0 0;
+  width: 800px;
+
+  &:first-child {
+    margin: 0;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+`;
+
+export const MyCartPageItemImg = styled.img`
+  width: 180px;
+  background-color: #f2e8da;
+  border-radius: 0.5rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100px;
+  }
+`;
+
+export const MyCartPageItemInfoBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-left: 1rem;
+  padding: 1rem;
+
+  @media screen and (max-width: 768px) {
+    padding: 0;
+  }
+`;
+
+export const MyCartPageItemTitle = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1.2rem;
+  }
+`;
+
+export const MyCartPageItemSize = styled.p`
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #ffb2a5;
+  margin: 1rem 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+export const MyCartPageItemPrice = styled.p`
+  font-size: 1.2rem;
+  margin: 0;
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
+`;
+
+export const MyCartPageItemControlButton = styled.button`
+  border: none;
+  background-color: transparent;
+  padding: 0;
+  width: 1.5rem;
+  height: 1.5rem;
+  cursor: pointer;
+
+  & > svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
+  &:last-child {
+    margin-left: 1rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 1.2rem;
+    height: 1.2rem;
+
+    & > svg {
+      width: 1.2rem;
+      height: 1.2rem;
+    }
+  }
+`;
+
+export const MyCartPageItemControlBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: auto;
+`;
+
+export const MyCartPageItemCount = styled.p`
+  font-size: 1rem;
+  margin: 0 1rem;
+`;
+
+export const MyCartPageOrderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MyCartPageOrderBox = styled.div`
+  width: 800px;
+  padding: 1rem;
+  border: 2px solid #eee;
+  border-radius: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+  margin: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: calc(100% - 2rem);
+    margin-bottom: 100px;
+  }
+`;
+
+export const MyCartPageOrderFlexBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0.5rem 0;
+
+  &:first-child {
+    margin-top: 0;
+  }
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+
+export const MyCartPageOrderPriceText = styled.p`
+  margin: 0;
+  font-size: 1rem;
+`;
+
+export const MyCartPageOrderTotalPriceText = styled.p`
+  margin: 0;
+  font-size: 1.2rem;
+  font-weight: bold;
+`;
+
+export const MyCartPageDivider = styled.div`
+  border: 1px solid #eee;
+`;
+
+export const MyCartPageOrderButton = styled.button`
+  width: 500px;
+  border: none;
+  border-radius: 0.5rem;
+  background-color: #ffbe93;
+  font-size: 1rem;
+  font-weight: bold;
+  color: white;
+  padding: 1rem 0;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #fa9a85;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: calc(100% - 2rem);
+    font-size: 0.8rem;
+    position: fixed;
+    bottom: 2rem;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+`;
