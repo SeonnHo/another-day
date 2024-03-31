79f26ad01609b43baf8080596d1469e6f76ee71a
SeonnHo <prefer914@gmail.com>
2024-03-31 23:11:50 +0900

    QueryClientProvider 컴포넌트에 Header 컴포넌트 포함 및 ReactQueryDevtools 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header/Header';
-import { AuthContextProvider } from './components/context/AuthContext';
+import { AuthContextProvider } from './context/AuthContext';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,10 +14,9 @@ const queryClient = new QueryClient({
 export default function App() {
   return (
     <AuthContextProvider>
-      <Header />
       <QueryClientProvider client={queryClient}>
+        <Header />
         <Outlet />
-        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </AuthContextProvider>
   );
